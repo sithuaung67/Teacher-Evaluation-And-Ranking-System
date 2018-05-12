087725d780206f83fc5f2d5763188fe7df650b37
MerrySean <Zilex.Seryth@gmail.com>
2018-05-13 03:51:57 +0800

    create Subjects page

---
+++ resources/assets/js/pages/subjects/subjects.vue
@@ -0,0 +1,41 @@
+<template>
+  <b-jumbotron header="Subjects">
+
+  </b-jumbotron>
+</template>
+
+<script>
+    export default {
+        middleware: 'auth',
+
+        name: 'subjects',
+
+        mixins: [],
+
+        data () {
+            return {
+                //
+            }
+        },
+
+        props: {
+            //
+        },
+
+        computed: {
+            //
+        },
+
+        created () {
+            //
+        },
+
+        mounted () {
+            //
+        },
+
+        methods: {
+            //
+        }
+    };
+</script>
