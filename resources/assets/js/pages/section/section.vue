9f3c27273d4f377d0a75f50401ae3fe9e46534f3
MerrySean <Zilex.Seryth@gmail.com>
2018-05-13 03:51:47 +0800

    create sections page

---
+++ resources/assets/js/pages/section/section.vue
@@ -0,0 +1,42 @@
+<template>
+    <b-jumbotron header="Section">
+
+    </b-jumbotron>
+</template>
+
+<script>
+    export default {
+
+        middleware: 'auth',
+
+        name: 'Section',
+
+        mixins: [],
+
+        data () {
+            return {
+                //
+            }
+        },
+
+        props: {
+            //
+        },
+
+        computed: {
+            //
+        },
+
+        created () {
+            //
+        },
+
+        mounted () {
+            //
+        },
+
+        methods: {
+            //
+        }
+    };
+</script>
