f210cfa69a9d12fc7507136caa3d1b13b6e052c9
MerrySean <Zilex.Seryth@gmail.com>
2018-06-16 08:09:51 +0800

    added Domain routing with middlewares base on domain

--- routes/api.php
+++ routes/api.php
@@ -13,23 +13,26 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::group(['middleware' => 'auth:api'], function () {
-    Route::post('logout', 'Auth\LoginController@logout');
-
-    Route::get('/user', function (Request $request) {
-        return $request->user();
+    Route::domain('guest.Tears.test')->middleware('guest')->group(function () {
+        // -- /api/login
+        Route::post('login', 'Auth\LoginController@login');
+        // -- /api/register
+        Route::post('register', 'Auth\RegisterController@register');
+        // -- /api/password/email
+        Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+        // -- /api/password/reset
+        Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+        // -- /api/oauth/
+        // Route::post('oauth/{driver}', 'Auth\OAuthController@redirectToProvider');
+        // Route::get('oauth/{driver}/callback', 'Auth\OAuthController@handleProviderCallback')->name('oauth.callback');
+    
     });
-
-    Route::patch('settings/profile', 'Settings\ProfileController@update');
-    Route::patch('settings/password', 'Settings\PasswordController@update');
-});
-
-Route::group(['middleware' => 'guest'], function () {
-    Route::post('login', 'Auth\LoginController@login');
-    Route::post('register', 'Auth\RegisterController@register');
-    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
-    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
-
-    Route::post('oauth/{driver}', 'Auth\OAuthController@redirectToProvider');
-    Route::get('oauth/{driver}/callback', 'Auth\OAuthController@handleProviderCallback')->name('oauth.callback');
-});
+    
+    Route::domain('admin.Tears.test')->middleware('auth:admin')->group(function () {
+        Route::post('logout', 'Auth\LoginController@logout');
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+        Route::patch('settings/profile', 'Settings\ProfileController@update');
+        Route::patch('settings/password', 'Settings\PasswordController@update');
+    });
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,36 @@
 |
 */
 
-Route::get('{path}', function () {
-    return view('index');
-})->where('path', '(.*)');
+Route::domain('teacher.Tears.test')->middleware('auth:teacher')->group(function () {
+    Route::get('{path}', function () {
+        return view('teacher');
+    })->where('path', '(.*)');
+});
+Route::domain('student.Tears.test')->middleware('auth:student')->group(function () {
+    Route::get('{path}', function () {
+        return view('student');
+    })->where('path', '(.*)');
+});
+Route::domain('admin.Tears.test')->middleware('auth.basic')->group(function () {
+    Route::get('{path}', function () {
+        return view('admin');
+    })->where('path', '(.*)');
+});
+Route::domain('guest.Tears.test')->middleware('guest')->name("guest")->group(function () {
+    Route::get('{path}', function () {
+        return view('guest');
+    })->where('path', '(.*)');
+});
+
+Route::domain('Tears.test')->middleware('guest')->group(function () {
+    Route::get('{path}', function () {
+        return redirect()->route('guest',['path' => '/']);
+    })->where('path', '(.*)');
+});
+
+
+Route::domain('guest.Tears.test')->middleware('guest')->group(function () {
+    Route::get('/login', function () {
+        return view('guest');
+    })->name("login");
+});
