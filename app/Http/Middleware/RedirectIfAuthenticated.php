8fc721d35e22fd90cd40125bb10515c96e20c8da
MerrySean <Zilex.Seryth@gmail.com>
2018-06-16 08:02:04 +0800

    redirect to guest but if logged-in then redirect base on user tpye

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -15,11 +15,24 @@ class RedirectIfAuthenticated
      * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next, $guard = null)
+    public function handle($request, Closure $next)
     {
-        if (Auth::guard($guard)->check()) {
-            return response()->json(['error' => 'Already authenticated.'], 400);
+         // To check if user is logged in as admin
+        if (Auth::guard('admin')->check()) {
+            return  redirect()->route('admin');
         }
+        // To check if user is logged in as student
+        if (Auth::guard('student')->check()) {
+            return  redirect()->route('student');
+        }
+        // To check if user is logged in as teacher
+        if (Auth::guard('teacher')->check()) {
+            return  redirect()->route('student');
+        }
+        // To check if user is a guess
+        // if (Auth::guest()) {
+        //     return  redirect()->route('login');
+        // }
 
         return $next($request);
     }
